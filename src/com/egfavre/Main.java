bff6b8afb942496875d8d2b86523b0b708d156e0
Liz Favre <egfavre@gmail.com>
2016-05-27 15:41:00 -0400

    all basic requirements are running

--- src/com/egfavre/Main.java
+++ src/com/egfavre/Main.java
@@ -1,16 +1,21 @@
 package com.egfavre;
 
+import jodd.json.JsonSerializer;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 public class Main {
+    public static String answer;
 
     public static void main(String[] args) throws FileNotFoundException {
-        //sort the objects into array by first character
+
         //ask user to pick a letter; throw exception if they don't pick a valid entry
         //save a file of countries that start with that letter
         //Optional Tasks
@@ -45,25 +50,41 @@ public class Main {
             allCountries.get(firstLetterStr).add(name);
         }
 
-        System.out.println(allCountries);
+        Scanner consoleScanner = new Scanner(System.in);
 
+        getLetter();
 
-       /*Scanner consoleScanner = new Scanner(System.in);
+        ArrayList<String> choosenCountryList = allCountries.get(answer);
+        String filename = answer + "_countries.txt";
 
-        //getLetter();
+        save(filename, choosenCountryList);
+
         }
 
         public static String getLetter() {
             Scanner consoleScanner = new Scanner(System.in);
             System.out.println("Please pick a letter");
-            String answer = consoleScanner.nextLine();
+            answer = consoleScanner.nextLine();
             char[] charAnswer = answer.toCharArray();
             if (charAnswer.length != 1 || !Character.isAlphabetic(charAnswer[0])) {
             System.out.println("That is not a single letter.");
                 getLetter();
             }
-            return answer;*/
+            return answer;
+
         }
 
+    public static void save(String filename, ArrayList<String> choosenCountryList) {
+        JsonSerializer serializer = new JsonSerializer();
+        String json = serializer.include("*").serialize(choosenCountryList);
+        File f = new File(filename);
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(json);
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+}
 
